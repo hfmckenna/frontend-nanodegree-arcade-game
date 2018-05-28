c589dd854232f1302e0fe6a782199f18ade1ffb1
hfmckenna <hf.mckenna@gmail.com>
2018-05-28 17:13:24 +0100

    Code beautified.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-const Enemy = function() {
+const Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -14,11 +14,11 @@ const Enemy = function() {
 // Used for enemy speed, placeholder code from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomArbitrary() {
     return Math.random() * (350 - 150) + 150;
-  };
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -26,16 +26,14 @@ Enemy.prototype.update = function(dt) {
         this.x = -101;
         this.speed = getRandomArbitrary() + (player.score * 5);
     } else {
-    this.x += dt * this.speed;
+        this.x += dt * this.speed;
     }
-    if (this.y === player.y)
-    {if (this.x > player.x && this.x < (player.x + 73) || this.x < player.x && (this.x + 83) > player.x) 
-        {
+    if (this.y === player.y) {
+        if (this.x > player.x && this.x < (player.x + 73) || this.x < player.x && (this.x + 83) > player.x) {
             player.lives -= 1;
             if (player.lives > 0) {
                 resetGame();
-            }
-            else {
+            } else {
                 gameOver();
             }
         }
@@ -43,7 +41,7 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -51,7 +49,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-const Player = function() {
+const Player = function () {
     this.sprite = 'images/char-boy.png';
     this.x = 202;
     this.y = 390;
@@ -59,36 +57,37 @@ const Player = function() {
     this.score = 0;
 };
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function (dt) {
     // Restricts movement of the player on the grid.
-    this.x = Math.min(404,Math.max(0,this.x));
-    this.y = Math.min(390,Math.max(-25,this.y));
-    if (this.y < 57)
-    {this.score += 1;
-        resetGame();}
+    this.x = Math.min(404, Math.max(0, this.x));
+    this.y = Math.min(390, Math.max(-25, this.y));
+    if (this.y < 57) {
+        this.score += 1;
+        resetGame();
+    }
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     //Bit janky to add Lives and Score here for drawing but they are both player related and saves modifying the Engine code. Might be worth a quick perf check.
     document.getElementById('gameInfo').textContent = `Lives: ${player.lives} Score: ${player.score}`
 };
 
-Player.prototype.handleInput = function(e) {
+Player.prototype.handleInput = function (e) {
     // Might be able to come up with object based solution rather than switch casing but somewhat unsure of this targeting.
     switch (e) {
-        case 'left' :
-        this.x -= 101
-        break;
-        case 'right' :
-        this.x += 101
-        break;
-        case 'up' :
-        this.y -= 83
-        break;
-        case 'down' :
-        this.y += 83
-        break;
+        case 'left':
+            this.x -= 101
+            break;
+        case 'right':
+            this.x += 101
+            break;
+        case 'up':
+            this.y -= 83
+            break;
+        case 'down':
+            this.y += 83
+            break;
     }
 };
 
@@ -106,11 +105,11 @@ generateEnemies();
 // Could maybe use spread operator for more succinct code but suspect push method will let me increase the difficulty as time goes on.
 
 function generateEnemies() {
-for (let i = 1; i <= 3; i++) {
-    const enemy = new Enemy();
-    allEnemies.push(enemy);
-    enemy.y = enemy.y + (83 * i);
-}
+    for (let i = 1; i <= 3; i++) {
+        const enemy = new Enemy();
+        allEnemies.push(enemy);
+        enemy.y = enemy.y + (83 * i);
+    }
 };
 
 let player = new Player();
@@ -118,7 +117,7 @@ let player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -139,17 +138,17 @@ function restartGame() {
     generateEnemies();
     player = new Player;
     const deleteButton = document.getElementById('resetBtn');
-    deleteButton.outerHTML= "";
+    deleteButton.outerHTML = "";
 }
 
-function gameOver () {
+function gameOver() {
     //Empty functions used on player death to "stop" the game, without producing errors in the engine or reloading anything.
     //This could be achieved with cancelAnimationFrame in Engine but that has it's own set of issues.
-    player.update = function(){};
-    for (let i = 0; i < allEnemies.length; i++){
-        allEnemies[i].update = function (){};
+    player.update = function () {};
+    for (let i = 0; i < allEnemies.length; i++) {
+        allEnemies[i].update = function () {};
     }
-    player.handleInput = function(){};
+    player.handleInput = function () {};
     const resetButton = document.createElement('button');
     resetButton.textContent = 'You Died. Try Again?';
     resetButton.id = 'resetBtn';
